3812ecee2c512f32bf33df374f1a2164bd96a320
Alexandra Rabchenya <arabchenya@gmail.com>
2025-01-25 18:29:06 +0300

    Добавлен основной код

---
+++ main.py
@@ -0,0 +1,8 @@
+from utils import add_numbers, multiply_numbers
+if __name__ == "__main__":
+  a = 3
+  b = 6
+  result_sum = add_numbers(a, b)
+  result_mul = multiply_numbers(a, b)
+  print(f"Сумма a и b: {sum_result}")
+  print(f"Произведение a и b: {result_num}")
