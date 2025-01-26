805685291455d0541304277135ffba3983ae9d9b
Alexandra Rabchenya <arabchenya@gmail.com>
2025-01-26 14:09:26 +0300

    Добавлена ноая функция add_numbers2

--- utils.py
+++ utils.py
@@ -1,2 +1,8 @@
 def multiply_numbers(a, b):
   return a * b
+def add_numbers2(*args):
+ total = sum(args)
+ rerurn total
+#from math import prod
+#numbers массив, например numbers = [2, 3, 4]
+result_mulpiply = prod(numbers)
